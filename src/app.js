c51ad646887784cf640aa804af3ace4b021c0cb0
jinwoo6531 <jintakk6531@gmail.com>
2022-01-26 14:27:46 +0900

    test

--- src/app.js
+++ src/app.js
@@ -1,31 +1,27 @@
 /* @jsx createElement */
-import {Component, createElement, render} from './react';
-
+import { Component, createElement, render } from './react';
 
 class Title extends Component {
-    render() {
-        return <h1> {this.props.children} </h1>
-    }
+  render() {
+    return <h1> {this.props.children} </h1>;
+  }
 }
 
-
-//함수 컴포넌트
+//함수 컴포넌트 / 클래스 컴포넌트
 function Item(props) {
-    return <li style={`color:${props.color}`}>{props.children}</li>
+  return <li style={`color:${props.color}`}>{props.children}</li>;
 }
 
 //
 const App = () => (
-    <p>
-        <Title>React 잘 만들기</Title>
-        <ul>
-            <Item color="red">첫 번째 아이템</Item>
-            <Item color="green">두 번째 아이템</Item>
-            <Item color="blue">세 번째 아이템</Item>
-        </ul>
-    </p>
+  <p>
+    <Title>React 잘 만들기</Title>
+    <ul>
+      <Item color="red">첫 번째 아이템</Item>
+      <Item color="green">두 번째 아이템</Item>
+      <Item color="blue">세 번째 아이템</Item>
+    </ul>
+  </p>
 );
 
-render(<App/>, document.querySelector('#root'));
-
-
+render(<App />, document.querySelector('#root'));

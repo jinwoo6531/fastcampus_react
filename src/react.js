800f46a0f6ecb402bad7adbece7954388eaee8b7
jinwoo6531 <jintakk6531@gmail.com>
2022-01-27 15:06:48 +0900

    클로저

--- src/react.js
+++ src/react.js
@@ -1,55 +1,63 @@
 export class Component {
-    constructor(props) {
-        this.props = props;
-    }
+  constructor(props) {
+    this.props = props;
+  }
 }
 
-
 export function createDOM(node) {
-    if (typeof node === 'string') {
-        return document.createTextNode(node);
-    }
+  if (typeof node === 'string') {
+    return document.createTextNode(node);
+  }
 
-    const element = document.createElement(node.tag);
+  const element = document.createElement(node.tag);
 
-    Object.entries(node.props)
-        .forEach(([name, value]) => element.setAttribute(name, value));
+  Object.entries(node.props).forEach(([name, value]) =>
+    element.setAttribute(name, value)
+  );
 
-    node.children.map(createDOM).forEach(element.appendChild.bind(element));
+  node.children.map(createDOM).forEach(element.appendChild.bind(element));
 
-    return element;
+  return element;
 }
 
 function makeProps(props, children) {
-    return {
-        ...props,
-        children: children.length === 1 ? children[0] : children
-    }
+  return {
+    ...props,
+    children: children.length === 1 ? children[0] : children,
+  };
 }
 
 export function createElement(tag, props, ...children) {
-    props = props || {};
+  props = props || {};
 
-
-    if (typeof tag === 'function') {
-        if (tag.prototype instanceof Component) {
-            const instance = new tag(makeProps(props, children));
-            return instance.render();
-        } else {
-            if (children.length > 0) {
-                return tag(makeProps(props, children));
-            } else {
-                return tag(props);
-            }
-        }
-
-
+  if (typeof tag === 'function') {
+    if (tag.prototype instanceof Component) {
+      const instance = new tag(makeProps(props, children));
+      return instance.render();
     } else {
-        return {tag, props, children};
+      if (children.length > 0) {
+        return tag(makeProps(props, children));
+      } else {
+        return tag(props);
+      }
     }
-
+  } else {
+    return { tag, props, children };
+  }
 }
 
 export function render(vdom, container) {
-    container.appendChild(createDOM(vdom));
+  container.appendChild(createDOM(vdom));
 }
+
+//클로저 학습
+export const render = (function () {
+  let prevDom = null;
+
+  return function (vdom, container) {
+    if (prevDom === null) {
+      prevDom = vdom;
+    }
+    container.appendChild(createDOM(vdom));
+  };
+})();
